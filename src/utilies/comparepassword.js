baddd95d477f7a4b40ad4e17fe0832aec77aca78
Barny Victor <victorbarny4@gmail.com>
2022-11-04 11:59:21 +0100

    deleted the utilis folder

--- src/utilies/comparepassword.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const validPassword = async (password, hashedPassword) => {
-    const userPassword = await bcrypt.compare(password, hashedPassword);
-    return userPassword;
-};
-
-module.exports = { validPassword };
